38e103ec246f98fba15cdb5f0759005b49506911
stepbot <stephanbotes@gmail.com>
2018-04-25 13:26:15 -0400

    added data folder

--- dataCreator.py
+++ dataCreator.py
@@ -3,10 +3,10 @@ from vrp import OrderGenerator
 
 trucks = [{"id":0,"capacity":5,"maxOrdersPerRun":2,"cost":60},{"id":1,"capacity":5,"maxOrdersPerRun":2,"cost":60},{"id":2,"capacity":10,"maxOrdersPerRun":2,"cost":90}]
 
-with open('trucks.json','w') as outfile:
+with open('data/trucks.json','w') as outfile:
     json.dump(trucks,outfile)
 
 orders = OrderGenerator(10,10,35)
 
-with open('orders.json','w') as outfile:
+with open('data/orders.json','w') as outfile:
     json.dump(orders,outfile)
