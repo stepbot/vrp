82ab5105febf9e1be09e42143c752e425926b058
stepbot <stephanbotes@gmail.com>
2018-04-27 10:53:15 -0400

    tester for new schedule runner

---
+++ scheduleRunnerTest.py
@@ -0,0 +1,17 @@
+import vrp
+import json
+
+with open('data/orders.json') as infile:
+    orders = json.load(infile)
+
+with open('data/trucks.json') as infile:
+    trucks = json.load(infile)
+
+
+schedule = vrp.RandomRouter(trucks,orders)
+simulatedSchedule = vrp.SimpleScheduleRunner(schedule,True)
+
+print('schedule passes validation? ', vrp.SimpleScheduleValidator(simulatedSchedule,orders))
+vrp.PrettyPrintSchedule(simulatedSchedule)
+print('total cost of schedule: $',simulatedSchedule['totalCost'])
+print('total time of schedule: ',simulatedSchedule['requiredTime'],'h')
